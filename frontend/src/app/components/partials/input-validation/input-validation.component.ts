44443b9744f758d34f394c99c57303c0bf3751b5
Paulo Henrique dos Santos Eiterer <pheiterer@hotmail.com>
2025-04-05 14:08:56 -0300

    Implement login flow

---
+++ frontend/src/app/components/partials/input-validation/input-validation.component.ts
@@ -0,0 +1,46 @@
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { AbstractControl } from '@angular/forms';
+
+const VALIDATOR_MESSAGE:any = {
+  required: 'This field is required',
+  email: 'Please enter a valid email address',
+}
+
+@Component({
+  selector: 'input-validation',
+  templateUrl: './input-validation.component.html',
+  styleUrls: ['./input-validation.component.css']
+})
+export class InputValidationComponent implements OnInit, OnChanges {
+  @Input()
+  control!: AbstractControl;
+  @Input()
+  showError: boolean = true;
+  errorMessages: string[] = [];
+  constructor() { }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    this.checkValidation();
+  }
+
+  ngOnInit(): void {
+    this.control.statusChanges.subscribe(() => {
+      this.checkValidation();
+    });
+
+    this.control.valueChanges.subscribe(() => {
+      this.checkValidation();
+    });
+  }
+
+  checkValidation(){
+    const errors = this.control.errors;
+    if (!errors){
+      this.errorMessages = [];
+      return;
+    }
+
+    const errorKey = Object.keys(errors);
+    this.errorMessages = errorKey.map(key => VALIDATOR_MESSAGE[key]);
+  }
+}
